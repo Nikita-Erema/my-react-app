f8f1521ce7e97050ba0240d558d75d72c7f49101
Nikita <k0bana@mail.ru>
2026-02-25 15:58:16 +0300

    com

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -4,8 +4,23 @@ import './main.css';
 import { AboutUsComponent } from "./AboutUs/AboutUs";
 import { HowWorkComponent } from "./HowDoesThisWork/HowWorkComponent";
 import { ReviewsComponent } from "./Reviews/Reviews";
+import { useCallback } from "react";
 
 export function MainPage() {
+
+  let a = 1;
+  const memoizedCallback = useCallback(
+    () => {
+      a += 1;
+    },
+    [a],
+  );
+
+  a = 2;
+
+  console.log(memoizedCallback());
+  
+
   return (
     <>
       <Header></Header>
